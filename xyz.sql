73cfeca6086abb445ca498436d4bbbb35f6e936b
Bhakti Bhoskar <bhoskarbhakti@gmail.com>
2025-09-24 07:37:15 +0530

    Update xyz.sql

--- xyz.sql
+++ xyz.sql
@@ -5,4 +5,16 @@ BEGIN
 INSERT INTO audit_log(srno, name)
 VALUES (:OLD.srno, :OLD.name);
 END;
+
+/
+
+
+  
+create or replace trigger errortrigger
+before delete or update on auditt
+begin
+if rtrim(upper(To_char(sysdate,'day')))='WEDNESDAY' then
+RAISE_APPLICATION_ERROR(-200000,'cannot perform delete or upadte operation');
+end if;
+end;
 /
